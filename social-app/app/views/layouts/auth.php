cc67f8e5ba86a9f75850e1449e58c0a1fe912d07
Hoang Nhat Nguyen <nhatnguyen.31231026537@st.ueh.edu.vn>
2026-03-31 21:18:54 +0700

    update css

--- social-app/app/views/layouts/auth.php
+++ social-app/app/views/layouts/auth.php
@@ -9,6 +9,7 @@
     <link rel="icon" type="image/png" sizes="16x16" href="<?= BASE_URL ?>/public/images/favicon-16x16.png">
     <link rel="apple-touch-icon" href="<?= BASE_URL ?>/public/images/favicon-32x32.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="<?= BASE_URL ?>/public/css/style.css" rel="stylesheet">
     <link href="<?= BASE_URL ?>/public/css/auth.css" rel="stylesheet">
 </head>
 <body class="auth-page-bg">
